4bd4c91688fc49f87f6acf829768237f14a8d9fa
ukarpenkov <truloveu@gmail.com>
2023-09-21 17:51:45 +0300

    type for props

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,17 @@
+interface TodoItemProps {
+  id: string
+  title: string
+  completed: boolean
+}
+
+const TodoItem = ({ id, title, completed }: TodoItemProps) => {
+  return (
+    <li>
+      <input type="checkbox" checked={completed} />
+      <span>{title}</span>
+      <span>&times;</span>
+    </li>
+  )
+}
+
+export default TodoItem
