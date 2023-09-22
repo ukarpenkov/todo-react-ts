d5a95dd37c4a1c78157d1f81035ccacd69667ec3
ukarpenkov <truloveu@gmail.com>
2023-09-22 15:18:45 +0300

    style type

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import TodoItem from './components/TodoItem'
 function App() {
   return (
     <div className="App">
-      <TodoItem id="112" completed={false} title="Hello" />
+      <TodoItem
+        id="112"
+        completed={false}
+        title="Hello"
+        style={{ border: '1px solid black' }}
+      />
     </div>
   )
 }
