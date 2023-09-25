036424661a639fc8821f3a4062dd5ebe55452dcc
ukarpenkov <truloveu@gmail.com>
2023-09-25 22:36:01 +0300

    state types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import TodoItem from './components/TodoItem'
 
+type ITodo = {
+  id: string
+  title: string
+  completed: boolean
+}
 function App() {
+  const [text, setText] = useState('')
+  const [todos, setTodos] = useState<string[]>([])
+  const [] = useState<ITodo>({} as ITodo)
+
   return (
     <div className="App">
       <TodoItem
